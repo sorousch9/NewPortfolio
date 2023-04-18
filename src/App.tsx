153d4410157f7623a89c66303739a0ded0fe6570
Sorousch <soroush.raw@gmail.com>
2023-04-18 18:37:21 +0200

    Add tsconfig.json and update App.tsx and index.tsx
    Add tsconfig.json and update related files
    to migrate React codebase to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import "./App.css";
 import { Abilities } from "./components/abilities/Abilities";
 import { Banner } from "./components/banner/Banner";
 import { NavBar } from "./components/navBar/NavBar";
-import { Missions } from "./components/missions/Missions"
+import { Missions } from "./components/missions/Missions";
 import { Contact } from "./components/contact/Contact";
 import { Footer } from "./components/Footer/Footer";
 
-
-
 function App() {
   return (
-    
     <div className="App">
       <NavBar />
       <Banner />
       <Abilities />
       <Missions />
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import { render } from "@testing-library/react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
